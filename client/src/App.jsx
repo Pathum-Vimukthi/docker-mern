0401109e4a4f57289ca4d38dc677f15fb10b9703
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-08-18 12:34:01 +0530

    chore: Update npm dependencies and configuration files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect} from 'react';
+import axios from 'axios';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users,setUsers] = useState([]);
+
+  //fetch the users
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/users').then((response)=>{
+      setUsers(response.data.data);
+    }).catch((error)=>{
+      console.error("There was an error fetching the users!",error);
+    });
+  },[]);
 
   return (
     <>
+      <h1>Users</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {users.map((user)=>(
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
